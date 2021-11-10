2af177236ef939bb049fa4664f5265135765cff1
rorymcdonnell <rorymcdonnell95@gmail.com>
2021-11-10 19:20:48 +0100

    created itemActions to refactor initial code

---
+++ src/itemActions.js
@@ -0,0 +1,36 @@
+const decreaseQuality = (item) => {
+  if (item.quality > 0 && item.quality < 80) {
+    item.quality = item.quality - 1;
+  }
+};
+
+const increaseQuality = (item) => {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+  }
+};
+
+const decreaseSellIn = (item) => {
+  if (item.name !== "Sulfuras, Hand of Ragnaros") {
+    item.sellIn -= 1;
+  }
+};
+
+const itemExpired = (item) => {
+  if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+    if (item.sellIn < 0) {
+      item.quality = 0;
+      return true;
+    }
+  }
+  if (item.sellIn < 0) {
+    return true;
+  }
+};
+
+module.exports = {
+  decreaseQuality,
+  increaseQuality,
+  decreaseSellIn,
+  itemExpired,
+};
